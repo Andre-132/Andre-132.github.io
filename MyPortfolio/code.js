7e74e062c18772f8916e8819e89816059796916c
Andre Albarado <analbarado1@gmail.com>
2025-03-22 15:41:25 -0500

    My Portfolio

--- MyPortfolio/code.js
+++ MyPortfolio/code.js
@@ -1,6 +1,8 @@
 function navigateToPage() {
     window.location.href = "AboutMe.html";
+    console.log("Hello");
 }
 function goToPage() {
     window.location.href = "Portfolio.html";
+    console.log("Hello Go To Page");
 }
